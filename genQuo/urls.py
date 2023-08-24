f81177eeabf3e104bab889a540e7c51387024f4a
nullHawk <suryanshsinghshakya1@gmail.com>
2023-08-24 14:24:12 +0530

    .

--- genQuo/urls.py
+++ genQuo/urls.py
@@ -26,5 +26,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
